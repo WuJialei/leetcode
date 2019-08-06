0f4dc0ab32515c6202871615974023748a660068
wujialei <42853741+WuJialei@users.noreply.github.com>
2019-08-06 13:59:56 +0800

    *378. Kth Smallest Element in a Sorted Matrix
     *378. Kth Smallest Element in a Sorted Matrix  + java

---
+++ 378.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
+        	return 0;
+        }
+        int n = matrix.length;
+        int left = matrix[0][0];
+        int right = matrix[n-1][n-1];
+        while(left <= right){
+        	int cnt = 0;
+        	int mid = left + (right - left)/2;
+        	for(int i = 0; i < n; ++i){
+        		for(int j = 0; j < n; ++j){
+        			if(matrix[i][j] <= mid){
+        				++cnt;
+        			}
+        		}
+        	}
+        	if(cnt < k){
+        		left = mid + 1;
+        	}
+        	else{
+        		right = mid - 1;
+        	}
+        }
+        return left;
+    }
+}
