9d46706d4324de7000c2f482dadea700cca459ff
wujialei <42853741+WuJialei@users.noreply.github.com>
2019-09-10 14:26:08 +0800

    update 160.java

--- 160.java
+++ 160.java
@@ -11,31 +11,57 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        /*
         if(headA == null || headB == null){
         	return null;
         }
         ListNode s1 = headA;
         ListNode s2 = headB;
         int cnt1 = 0, cnt2 = 0;
-        while(s1 != s2){
-        	if(cnt1 > 1 || cnt2 > 1){
-        		return null;
-        	}
-        	if (s1.next==null) {
-        		s1 = headB;
-        		++cnt1;
-        	}
-        	else{
-        		s1 = s1.next;
-        	}
-        	if (s2.next==null) {
-        		s2 = headA;
-        		++cnt2;
-        	}
-        	else{
-        		s2 =s2.next;
-        	}
+        while(s1 != null) {
+            ++cnt1;
+            s1 = s1.next;
         }
-        return s1;
+        
+        while(s2 != null) {
+            ++cnt2;
+            s2 = s2.next;
+        }
+        
+        int cnt = cnt1 - cnt2;
+        if (cnt > 0) {
+            while(cnt > 0) {
+                headA = headA.next;
+                --cnt;
+            }
+        } else if (cnt < 0) {
+            while(cnt < 0) {
+                headB = headB.next;
+                ++cnt;
+            }
+        }
+        while(headA != null && headB != null) {
+            if (headA == headB) {
+                return new ListNode(headA.val);
+            }
+            headA = headA.next;
+            headB = headB.next;
+        }  
+        return null;
+    }
+    */
+    
+        /**
+        定义两个指针, 第一轮让两个到达末尾的节点指向另一个链表的头部, 最后如果相遇则为交点(在第一轮移动中恰好抹除了长度差)
+        两个指针等于移动了相同的距离, 有交点就返回, 无交点就是各走了两条指针的长度
+        **/
+        if(headA == null || headB == null) return null;
+        ListNode pA = headA, pB = headB;
+        // 在这里第一轮体现在pA和pB第一次到达尾部会移向另一链表的表头, 而第二轮体现在如果pA或pB相交就返回交点, 不相交最后就是null==null
+        while(pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
     }
 }
