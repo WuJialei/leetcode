d82d24deafc4875f53a12cec5015103d5be8c147
wujialei <42853741+WuJialei@users.noreply.github.com>
2019-09-16 14:10:55 +0800

    328. Odd Even Linked List
    328. Odd Even Linked List + java

--- 328.java
+++ 328.java
@@ -1,4 +1,4 @@
-/**
+/* *
  * Definition for singly-linked list.
  * public class ListNode {
  *     int val;
@@ -6,9 +6,48 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+
+/* class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+ }
+ */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
         if (head == null || head.next == null) {
+            return head;
+        }
+        int cnt = 2;
+        ListNode evenList = null;
+        ListNode evenListIndex = null;
+        ListNode ret = head;
+        ListNode pre = head;
+        ListNode oddList = head.next;
+        while(oddList != null) {
+            if(cnt % 2 == 0) {
+                ListNode tmp = new ListNode(oddList.val);
+                if (evenList == null) {
+                    tmp.next = evenList;
+                    evenList = tmp;
+                    evenListIndex = evenList;
+                } else {
+                    evenList.next = tmp;
+                    evenList = evenList.next;
+                }
+                pre.next = oddList.next;
+                oddList = oddList.next;
+            } else {
+                pre = oddList;
+                oddList = oddList.next;
+            }
+            ++cnt;
+        }
+        pre.next = evenListIndex;
+        return ret;
+
+
+        /* if (head == null || head.next == null) {
         	return head;
         }
         ListNode oddNodes = new ListNode(head.val);
@@ -29,6 +68,7 @@ class Solution {
         	head = head.next;
         }
         oddNodes.next = evenNodesResult;
-        return oddNodesResult;
+        return oddNodesResult; */
+        
     }
 }
