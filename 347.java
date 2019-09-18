3b77fb7835cf94d1ac8b2ab128bf0ac30b247153
wujialei <42853741+WuJialei@users.noreply.github.com>
2019-09-18 17:26:45 +0800

    update 347.java

--- 347.java
+++ 347.java
@@ -1,6 +1,35 @@
+import java.util.Map;
+import java.util.PriorityQueue;
+
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+
 class Solution {
     public List<Integer> topKFrequent(int[] nums, int k) {
-        List<Integer> res = new ArrayList<Integer>();
+
+        List<Integer> ret = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        } 
+
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>((a, b) -> map.get(a) - map.get(b));
+
+        for (int key : map.keySet()) {
+            queue.offer(key);
+            if (queue.size() > k) {
+                queue.poll();
+            }
+        }
+
+        for (int v : queue) {
+            ret.add(v);
+        }
+
+        return ret;
+
+
+
+        /* List<Integer> res = new ArrayList<Integer>();
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         for(int i : nums){
         	map.put(i, map.getOrDefault(i, 0) + 1);
@@ -18,7 +47,7 @@ class Solution {
         		res.addAll(temp[t]);
         	}
         }
-        return res;
+        return res; */
 
     }
 }
