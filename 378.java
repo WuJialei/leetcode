bc76cad40aba73376d7684cf19484174bd2e18ea
wujialei <42853741+WuJialei@users.noreply.github.com>
2019-09-18 16:17:51 +0800

    update 378.java

--- 378.java
+++ 378.java
@@ -1,6 +1,38 @@
 class Solution {
+
+	public int find (int[][] arrays, int target) {
+		int n = arrays.length;
+		int i = n - 1, j = 0;
+		int num = 0;
+		while (j < n && i >= 0) {
+			if (arrays[i][j] > target) {
+				--i;
+			} else {
+				num += i + 1;
+				++j;
+			}
+		}
+		return num;
+	}
+
     public int kthSmallest(int[][] matrix, int k) {
-        if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
+
+		if (matrix == null || matrix.length == 0) {
+			return 0;
+		}
+		int n = matrix.length;
+		int low = matrix[0][0], high = matrix[n-1][n-1];
+		while (low < high) {
+			int mid = low + (high - low) / 2;
+			int cnt = find(matrix, mid);
+			if (cnt < k) {
+				low = mid + 1;
+			} else {
+				high = mid;
+			}
+		}
+		return low;
+        /* if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
         	return 0;
         }
         int n = matrix.length;
@@ -23,6 +55,6 @@ class Solution {
         		right = mid - 1;
         	}
         }
-        return left;
+        return left; */
     }
 }
