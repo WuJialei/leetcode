2805503f780a93ade12397f6f7e61aa655d6eb11
wujialei <42853741+WuJialei@users.noreply.github.com>
2019-09-29 21:54:27 +0800

    Update 287.java

--- 287.java
+++ 287.java
@@ -1,5 +1,6 @@
 class Solution {
     public int findDuplicate(int[] nums) {
+        /*
         int slow = 0;
         int fast = 0;
         do{
@@ -12,5 +13,23 @@ class Solution {
         	fast = nums[fast];
         }
         return slow;
+        */
+        int len = nums.length;
+        int left = 0, right = len - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int cnt = 0;
+            for (int tmp : nums) {
+                if (tmp <= mid) {
+                    ++cnt;
+                }
+            }
+            if (cnt <= mid) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
     }
 }
