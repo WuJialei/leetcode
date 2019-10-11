96d534bdf6a584c2f3ea7a2d845acbd38e9ce205
wujialei <42853741+WuJialei@users.noreply.github.com>
2019-10-11 15:26:35 +0800

    Update 198.java

--- 198.java
+++ 198.java
@@ -1,5 +1,6 @@
 class Solution {
     public int rob(int[] nums) {
+        /*
         if(nums == null || nums.length == 0){
         	return 0;
         }
@@ -11,5 +12,23 @@ class Solution {
         	profits[i] = Math.max(profits[i-1], profits[i-2] + nums[i-1]);
         }
         return profits[n];
+        */
+        int ans = 0;
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        int a = nums[0], b = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; ++i) {
+            ans = Math.max(nums[i] + a, b);
+            a = b;
+            b =ans;
+        }
+        return ans;
     }
 }
