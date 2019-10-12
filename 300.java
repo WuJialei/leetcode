7ada7d54e01fa193cc6441930c8fb6b15012d1cb
wujialei <42853741+WuJialei@users.noreply.github.com>
2019-10-12 15:14:28 +0800

    Update 300.java

--- 300.java
+++ 300.java
@@ -1,5 +1,30 @@
+import java.util.Arrays;
+
 class Solution {
     public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int len = nums.length;
+        if (len == 1) {
+            return 1;
+        }
+        int end = 0;
+        int[] tail = new int[len]; 
+        tail[0] = nums[0];
+        for (int i = 1; i < len; ++i) {
+            if (nums[i] > tail[end]) {
+                tail[++end] = nums[i];
+            } else {
+                int index = Arrays.binarySearch(tail, 0, end+1, nums[i]);
+                if (index < 0) {
+                    index = -index - 1;
+                } 
+                tail[index] = nums[i];
+            }
+        }
+        return end+1;
+        /*
         if(nums == null || nums.length == 0){
             return 0;
         }
@@ -20,5 +45,25 @@ class Solution {
             result = Math.max(result, t);
         }
         return result;
+        */
+        /*
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int len = nums.length;
+        int[] dp = new int[len];
+        Arrays.fill(dp, 1);
+        int ans = 1;
+        for (int i = 1; i < len; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+            ans = Math.max(ans, dp[i]);
+        }
+        return ans;
+        */
+       
     }
 }
