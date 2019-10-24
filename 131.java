98408d8f665fe6a5871cc7e76ea11471eb0906fd
wujialei <42853741+WuJialei@users.noreply.github.com>
2019-10-24 17:12:25 +0800

    Update 131.java

--- 131.java
+++ 131.java
@@ -1,15 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
 
+	private List<List<String>> ans;
+	private String s;
+
     public List<List<String>> partition(String s) {
+		this.s = s;
+		ans = new ArrayList<List<String>>();
+		List<String> ansItem = new ArrayList<String>();
+		dfs(ansItem, 0);
+		return ans;
+		/*
         List<List<String>> res = new ArrayList<List<String>>();
         if(s == null || s.length() == 0){
         	return res;
         }
         List<String> item = new ArrayList<String>();
         dfs(res, item, s);
-        return res;
+		return res;
+		*/
     }
 
+	public void dfs(List<String> item, int start) {
+		if (start == s.length()) {
+			ans.add(new ArrayList(item));
+			return;
+		}
+		for (int i = start; i < s.length(); ++i) {
+			if (isPartion(start, i)) {
+				item.add(s.substring(start, i+1));
+				dfs(item, i+1);
+				item.remove(item.size()-1);
+			}
+		}
+	}
+
+	public boolean isPartion(int begin, int end) {
+		while(begin < end) {
+			if(s.charAt(begin++) != s.charAt(end--)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+
+	/*
     public void dfs(List<List<String>> result, List<String> itemList, String pat){
     	if(pat.length() == 0){
     		result.add(new ArrayList(itemList));
@@ -31,7 +69,8 @@ class Solution {
     		}
     	}
     	return true;
-    }
+	}
+	*/
 
 
 }
