204300f98f2aa96a5fcf280608e4fea219645d98
wujialei <42853741+WuJialei@users.noreply.github.com>
2019-10-25 12:53:49 +0800

    Update 139.java

--- 139.java
+++ 139.java
@@ -1,5 +1,11 @@
 class Solution {
+
+    private String s;
+    private List<String> wordDict;
+    private int[] visited;
+
     public boolean wordBreak(String s, List<String> wordDict) {
+        /*
         int n = s.length();
         boolean[] dp = new boolean[n+1];
         dp[0] = true;
@@ -12,5 +18,32 @@ class Solution {
         	}
         }
         return dp[n];
+        */
+        this.s = s;
+        this.wordDict = wordDict;
+        visited = new int[s.length() + 1];
+        return dfs(0, s.length()-1);
     }
+
+    public boolean dfs (int start, int end) {
+        if (start > end) {
+            visited[start] = 1;
+            return true;
+        }
+        if (visited[start] != 0) {
+            return visited[start] == 1 ? true : false;
+        }
+        for (int i = start; i <= end; ++i) {
+            if (wordDict.contains(s.substring(start, i+1)) && dfs(i+1, end)) {
+                visited[start] = 1;
+                return true;
+            } else {
+                visited[start] = 2;
+            }
+
+        }
+        visited[start] = 2;
+        return false;
+    }
+
 }
