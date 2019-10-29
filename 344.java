633277d497cd45272b1d995bf19e7ad36fff5065
wujialei <42853741+WuJialei@users.noreply.github.com>
2019-10-29 11:25:09 +0800

    Create 344.java

---
+++ 344.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int start = 0, end = s.length-1;
+        char temp;
+        while (start < end) {
+            temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            ++start;
+            --end;
+        }
+    }
+}
