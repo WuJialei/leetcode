8346d387b0a0af650c25ab205bfb7debbe278d96
wujialei <42853741+WuJialei@users.noreply.github.com>
2019-11-24 15:07:24 +0800

    Update 496.java

--- 496.java
+++ 496.java
@@ -1,22 +1,25 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> myMap = new HashMap<Integer, Integer>();
-        Stack<Integer> myStack = new Stack<Integer>(); 
-        int[] result = new int[nums1.length];
-        for(int temp : nums2){
-        	while(!myStack.empty() && temp > myStack.peek()){
-        		myMap.put(myStack.pop(), temp);
-        	}
-        	myStack.push(temp);
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        Stack<Integer> stack = new Stack<Integer> ();
+        int[] ans = new int[nums1.length];
+        for (int num : nums2) {
+            if (stack.isEmpty()) {
+                stack.push(num);
+            } else {
+                // 单调栈是严格单调递减的
+                while (!stack.isEmpty() && stack.peek() < num) {
+                    map.put(stack.pop(), num);
+                }
+                stack.push(num);
+            }
         }
-        for(int i = 0; i < nums1.length; ++i){
-        	if(myMap.containsKey(nums1[i])){
-        		result[i] = myMap.get(nums1[i]);
-        	}
-        	else{
-        		result[i] = -1;
-        	}
+        while (!stack.isEmpty()) {
+            map.put(stack.pop(), -1);
         }
-        return result;
+        for (int i = 0; i < nums1.length; ++i) {
+            ans[i] = map.get(nums1[i]);
+        }
+        return ans;
     }
 }
