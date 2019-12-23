6f74ab607cbf3ec7c2d4154939995b76cff13321
wujialei <42853741+WuJialei@users.noreply.github.com>
2019-12-23 14:07:40 +0800

    [100] Same Tree
    [100] Same Tree

---
+++ 100.same-tree.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=100 lang=java
+ *
+ * [100] Same Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else {
+            if (p.val == q.val) {
+                return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+            } else {
+                return false;
+            }
+        }
+    }
+}
+// @lc code=end
+
