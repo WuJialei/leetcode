adc7a6aadf4c98ee2a4036b6a5fe85777cc3e3f5
wujialei <42853741+WuJialei@users.noreply.github.com>
2020-01-07 12:18:07 +0800

    [951] Flip Equivalent Binary Trees
    [951] Flip Equivalent Binary Trees

---
+++ 951.flip-equivalent-binary-trees.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=951 lang=java
+ *
+ * [951] Flip Equivalent Binary Trees
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        if (root1 == root2) {
+            return true;
+        }
+        if (root1 == null || root2 == null || root1.val != root2.val) {
+            return false;
+        }
+        // 判断左右子节点是否相等或者可以通过翻转相等
+        return (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)) || (flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left));
+    }
+}
+// @lc code=end
+
