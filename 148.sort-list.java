778b9c06fd20a6d986c8d971e0745c308d01e0c7
wujialei <42853741+WuJialei@users.noreply.github.com>
2020-03-03 10:49:34 +0800

    [148] Sort List
    [148] Sort List

---
+++ 148.sort-list.java
@@ -0,0 +1,50 @@
+import java.util.List;
+
+/*
+ * @lc app=leetcode id=148 lang=java
+ *
+ * [148] Sort List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode tmp = slow.next;
+        slow.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(tmp);
+        ListNode h = new ListNode(-1);
+        ListNode ans = h;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                h.next = left;
+                left = left.next;
+            } else {
+                h.next = right;
+                right = right.next;
+            }
+            h = h.next;
+        }
+        h.next = left!=null ? left : right;
+        return ans.next;
+    }
+}
+// @lc code=end
+
