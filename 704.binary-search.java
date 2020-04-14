19ba08d8f387d275a9f0c91822face2ea2d758fb
wujialei <42853741+WuJialei@users.noreply.github.com>
2020-04-14 10:42:18 +0800

    [704] Binary Search
    [704] Binary Search

---
+++ 704.binary-search.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=704 lang=java
+ *
+ * [704] Binary Search
+ */
+
+// @lc code=start
+class Solution {
+    public int search(int[] nums, int target) {
+        int len = nums.length;
+        int left = 0;
+        int right = len - 1;
+        while (left < right) {
+            int mid = left + (right - left)/2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left] == target ? left : -1;
+    }
+}
+// @lc code=end
+
