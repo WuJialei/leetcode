8fa835e99ae5476f5dd804d5edaee7919c7a5649
wujialei <42853741+WuJialei@users.noreply.github.com>
2020-05-06 11:48:09 +0800

    Update 110.balanced-binary-tree.java

--- 110.balanced-binary-tree.java
+++ 110.balanced-binary-tree.java
@@ -1,56 +1,29 @@
-/*
- * @lc app=leetcode id=110 lang=java
- *
- * [110] Balanced Binary Tree
- */
-
-// @lc code=start
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        getHeight(root);
-        return dfs(root);
-    }
-
-    public int getHeight(TreeNode node) {
-        if (node == null) {
-            return 0;
-        }
-        node.val = Math.max(getHeight(node.left), getHeight(node.right)) + 1;
-        return node.val;
-    }
-
-    public boolean dfs(TreeNode node) {
-        if (node == null) {
+        if (root == null) {
             return true;
         }
-        if (node.left == null && node.right == null) {
-            return true;
-        } else if (node.left == null || node.right == null) {
-            int tmp = node.left == null ? node.right.val : node.left.val;
-            if (tmp > 1) {
-                return false;
-            } else {
-                return true;
-            }
-        } else {
-            if (Math.abs(node.left.val - node.right.val) > 1) {
-                return false;
-            } else {
-                return dfs(node.left) && dfs(node.right);
-            }
+        if (!isBalanced(root.left) || !isBalanced(root.right)) {
+            return false;
         }
-        
+        int lHeight = root.left == null ? 0 : root.left.val;
+        int rHeight = root.right == null ? 0 : root.right.val;
+        root.val = 1 + Math.max(lHeight, rHeight);
+        return Math.abs(lHeight - rHeight) <= 1;
     }
- 
 }
-// @lc code=end
-
